1c119be642171745c2ef7995f20cff51b5defa0d
wearetyomsmnv <a7n000v@gmail.com>
2024-03-29 06:58:28 -0400

    add kubectl, mlflow, docker

--- train.py
+++ train.py
@@ -1,15 +1,24 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 
-# Загрузка датасета
+# Загружаем датасет
 iris = load_iris()
 X, y = iris.data, iris.target
 
-# Разбиение на обучающую и тестовую выборку
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-# Обучение модели
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
-# Оценка модели
-print(f"Accuracy: {clf.score(X_test, y_test)}")
+# Разбиваем на тренировочный и тестовый наборы
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2022)
+
+# Начинаем MLflow эксперимент
+with mlflow.start_run():
+
+    # Создаем и тренируем модель
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    
+    # Записываем параметры, метрики и модель
+    mlflow.log_param("test_size", 0.3)
+    mlflow.log_metric("accuracy", model.score(X_test, y_test))
+    mlflow.sklearn.log_model(model, "model")
